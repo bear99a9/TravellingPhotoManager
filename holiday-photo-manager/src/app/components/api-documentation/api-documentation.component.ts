ff3c8f7e3ce1f8b608200922f9e148a5551a51ab
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-12-04 23:55:47 +0000

    Swagger ui part working no style

--- holiday-photo-manager/src/app/components/api-documentation/api-documentation.component.ts
+++ holiday-photo-manager/src/app/components/api-documentation/api-documentation.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import SwaggerUI from 'swagger-ui';
 
 @Component({
   selector: 'app-api-documentation',
   templateUrl: './api-documentation.component.html',
   styleUrls: ['./api-documentation.component.css']
 })
-export class ApiDocumentationComponent implements OnInit {
+export class ApiDocumentationComponent implements AfterViewInit {
 
-  ngOnInit(): void {
-  }
-}
+  ngAfterViewInit() {
+    const ui = SwaggerUI({
+        url: 'https://schoolagreementapi-test.azurewebsites.net/swagger/v1/swagger.json',
+        domNode: document.getElementById('swagger'),
+    });
+}}
