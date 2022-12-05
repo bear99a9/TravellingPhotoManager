63ad661b790b4a3834443280bee332115a254482
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-12-05 00:22:47 +0000

    Iframe working as swagger ui no css working

--- holiday-photo-manager/src/app/components/api-documentation/api-documentation.component.ts
+++ holiday-photo-manager/src/app/components/api-documentation/api-documentation.component.ts
@@ -1,16 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import SwaggerUI from 'swagger-ui';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-api-documentation',
   templateUrl: './api-documentation.component.html',
   styleUrls: ['./api-documentation.component.css']
 })
-export class ApiDocumentationComponent implements AfterViewInit {
+export class ApiDocumentationComponent {
 
-  ngAfterViewInit() {
-    const ui = SwaggerUI({
-        url: 'https://schoolagreementapi-test.azurewebsites.net/swagger/v1/swagger.json',
-        domNode: document.getElementById('swagger'),
-    });
-}}
+}
